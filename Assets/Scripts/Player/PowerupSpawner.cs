9abe85a6e7a9bdecd7f081f1545461c089f7185f
Mark Tan <mtrhorange@hotmail.com>
2016-11-12 05:04:50 +0800

    Powerups and shit

---
+++ Assets/Scripts/Player/PowerupSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupSpawner : MonoBehaviour {
+
+	float spawnTimer = 30;
+
+	public GameObject shieldPrefab, splitPrefab, healPrefab, laserPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnTimer += Time.deltaTime;
+
+		if (spawnTimer >= 4f) {
+			int rand = Random.Range (0, 4);
+			if (rand == 0) {
+				GameObject power = (GameObject)Instantiate (shieldPrefab, transform.position, Quaternion.identity);
+
+			}
+			else if (rand == 1) {
+				GameObject power = (GameObject)Instantiate (splitPrefab, transform.position, Quaternion.identity);
+
+			}
+			else if (rand == 2) {
+				GameObject power = (GameObject)Instantiate (healPrefab, transform.position, Quaternion.identity);
+			
+			}
+			else if (rand == 3) {
+				GameObject power = (GameObject)Instantiate (laserPrefab, transform.position, Quaternion.identity);
+
+			}
+
+			spawnTimer = 0f;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Shield.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	public float health = 4f;
+	Player player;
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.GetComponent<EnemyLaser> ()) {
+			GetDamage (other.GetComponent<EnemyLaser> ().damage);
+		}
+		if (other.GetComponent<Enemy> ()) 
+			GetDamage (2);
+	}
+
+	void GetDamage(int i) {
+		health -= i;
+		if (health <= 0) {
+			player.DisableShieldPowerup ();
+		}
+	}
+}
