b1bdbb925da90d5887293b3b52841f002fd1e908
Mark Tan <mtrhorange@hotmail.com>
2016-11-11 23:42:21 +0800

    Mark changes

--- Assets/Scripts/Player/PlayerLaser.cs
+++ Assets/Scripts/Player/PlayerLaser.cs
@@ -5,11 +5,11 @@ public class PlayerLaser : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GetComponent<Rigidbody2D>().velocity = transform.up * Time.deltaTime * 600f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
