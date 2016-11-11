f859df180bea331b706dd3ee3f8bd9f0422c8350
Mark Tan <mtrhorange@hotmail.com>
2016-11-11 21:41:05 +0800

    Imported proj

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject eLaser;
+
+	internal bool weakened = false;
+	private int HP = 1;
+
+	private bool goLeft = false;
+
+	Camera camera;
+	Bounds cameraBounds;
+
+	// Use this for initialization
+	void Start () {
+
+		camera = GameObject.Find("MainCamera").GetComponent<Camera>();
+
+		float screenAspect = (float)Screen.width / (float)Screen.height;
+		float cameraHeight = camera.orthographicSize * 2;
+		cameraBounds = new Bounds (camera.transform.position, new Vector3 (cameraHeight * screenAspect, cameraHeight, 0));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (goLeft)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 0) * Time.deltaTime * 80f;
+			if (transform.position.x < cameraBounds.min.x)
+			{
+				goLeft = false;
+			}
+		}
+		else
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * Time.deltaTime * 80f;
+			if (transform.position.x > cameraBounds.max.x)
+			{
+				goLeft = true;
+			}
+		}
+	}
+
+
+	//called when this entity receives damage from another source
+	internal void GetDamage(int dmg)
+	{
+		HP -= dmg;
+		//check die
+		if (HP <= 0)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Camera camera;
+	public int playerNo = 1;
+
+	float moveSpeed = 1f;
+	int health = 10;
+
+	Bounds cameraBounds;
+	Sprite sprite;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("MainCamera").GetComponent<Camera> ();
+		float screenAspect = (float)Screen.width / (float)Screen.height;
+		float cameraHeight = camera.orthographicSize * 2;
+		cameraBounds = new Bounds (camera.transform.position, new Vector3 (cameraHeight * screenAspect, cameraHeight, 0));
+		sprite = GetComponent<SpriteRenderer> ().sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovementInput ();
+	}
+
+	void MovementInput() {
+		if (playerNo == 1) {
+			if (Input.GetButton ("LeftP1") && transform.position.x + sprite.bounds.min.x > cameraBounds.min.x) {
+				transform.position += transform.right * -1f * moveSpeed * 0.1f;
+			}
+			if (Input.GetButton("RightP1") && transform.position.x + sprite.bounds.max.x < cameraBounds.max.x) {
+				transform.position += transform.right * moveSpeed * 0.1f;
+			}
+		} else {
+			if (Input.GetButton ("LeftP1") && transform.position.x + sprite.bounds.min.x > cameraBounds.min.x) {
+				transform.position += transform.right * -1f * moveSpeed * 0.1f;
+			}
+			if (Input.GetButton("RightP1") && transform.position.x + sprite.bounds.max.x < cameraBounds.max.x) {
+				transform.position += transform.right * moveSpeed * 0.1f;
+			}
+		}
+
+	}
+
+	public void GetDamage(int dmg) {
+
+		health -= dmg;
+	}
+}
