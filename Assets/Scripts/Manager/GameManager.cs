1d86be544fa891e1a5beec3ec6a096e0b16f0319
Mark Tan <mtrhorange@hotmail.com>
2016-11-12 14:12:24 +0800

    gamemanager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,20 @@
 <<<<<<< HEAD
-=======
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
     public static GameManager instance;
 
     public GameObject player1, player2;
     public float sfxVolume, bgmVolume;
     public Slider sfxSlider, bgmSlider;
+
+	public bool isGameOver = false;
+	public bool isPaused = false;
+
     void Awake()
     {
         instance = this;
@@ -39,9 +41,64 @@ public class GameManager : MonoBehaviour
         bgmVolume = bgmSlider.value;
     }
 
-    public void loadMainMenu()
+	public void PauseGame() {
+
+		Time.timeScale = 0;
+		isPaused = true;
+	}
+
+	public void ResumeGame() {
+		Time.timeScale = 1.0f;
+		isPaused = false;
+	}
+
+	public void GameOver() {
+
+		isGameOver = true;
+	}
+
+	public void loadMainMenu()
+	{
+		SceneManager.LoadScene("Main Menu");
+	}
+}
+=======
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public GameObject player1, player2;
+    public float sfxVolume, bgmVolume;
+    public Slider sfxSlider, bgmSlider;
+    void Awake()
     {
-        SceneManager.LoadScene("Main Menu");
+        instance = this;
+    }
+	// Use this for initialization
+	void Start ()
+	{
+	    //sfxVolume = MenuManager.instance.sfxValue;
+	    //bgmVolume = MenuManager.instance.bgmValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setSFX()
+    {
+        sfxVolume = sfxSlider.value;
+    }
+
+    public void setBGM()
+    {
+        bgmVolume = bgmSlider.value;
     }
 }
->>>>>>> gameOver
+>>>>>>> ben
